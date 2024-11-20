b9252246528dc723225e6a6a25264e7751a16eae
daryakefir <141852517+daryakefir@users.noreply.github.com>
2024-11-20 13:19:19 +0800

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,21 +1,40 @@
 import random
 import threading
-from telebot import TeleBot, types
+import time
+from telebot import TeleBot, types, apihelper
 
 from service_functions import load_json_data, get_new_image
 
-bot = TeleBot(token='YOUR TOKEN')
+bot = TeleBot('YOUR TOKEN')
 
 
 questions = load_json_data('questions.json')
 user_data = {}
 timers = {}
+max_retries = 5
+
+
+def safe_send_message(chat_id, text):
+    """Функция для безопасной отправки сообщения с обработкой ошибок."""
+
+    retries = 0
+    while retries < max_retries:
+        try:
+            bot.send_message(chat_id, text)
+            return
+        except apihelper.ApiTelegramException as e:
+            if e.error_code == 429:
+                wait_time = int(e.result['parameters']['retry_after']) + 1
+                time.sleep(wait_time)
+                retries += 1
+            else:
+                break
 
 
 @bot.message_handler(commands=['newquiz'])
 def new_quiz(message):
     """Команда для начала нового квиза."""
-
+    
     chat_id = message.chat.id
     scores = 0
     questions_to_ask = random.sample(questions, 10)
@@ -29,10 +48,9 @@ def new_quiz(message):
 
 def ask_question(chat_id):
     """Функция для отправки вопроса пользователю."""
-
+    
     user_info = user_data.get(chat_id)
 
-    # Проверяем, остались ли еще вопросы для отправки
     if user_info and user_info['current_question_index'] < len(user_info['questions']):
         question = user_info['questions'][user_info['current_question_index']]
         options = [types.InlineKeyboardButton(option, callback_data=option) for option in question['options']]
@@ -40,24 +58,29 @@ def ask_question(chat_id):
         keyboard.add(*options)
 
         with open(question['pic'], 'rb') as photo:
-            bot.send_photo(
-                chat_id=chat_id,
-                photo=photo,
-                reply_markup=keyboard
-            )
+            retries = 0
+            while retries < max_retries:
+                try:
+                    bot.send_photo(chat_id=chat_id, photo=photo, reply_markup=keyboard)
+                    break  # Успешно отправлено, выходим
+                except apihelper.ApiTelegramException as e:
+                    if e.error_code == 429:
+                        wait_time = 5  # Задержка на 5 секунд (можно изменить)
+                        print(f"Бот превысил лимиты, ждем {wait_time} секунд")
+                        time.sleep(wait_time)
+                        retries += 1
+                    else:
+                        print(f"Ошибка при отправке фото: {e}")
+                        break
 
-        # Запускаем таймер
         if chat_id in timers:
             timers[chat_id].cancel()
         timers[chat_id] = threading.Timer(30, time_is_up, args=(chat_id,))
         timers[chat_id].start()
-
     else:
-        # Если вопросы закончились, отправляем результат и картинку с котиком
         result = user_info['scores']
-        bot.send_message(chat_id=chat_id,
-                         text=f'Your result is {result} from 10 🐥!')
-        bot.send_photo(chat_id, get_new_image())
+        safe_send_message(chat_id, f'Your result is {result} from 10 🐥!')
+        safe_send_message(chat_id, get_new_image())
         del user_data[chat_id]
         if chat_id in timers:
             timers[chat_id].cancel()
@@ -67,22 +90,19 @@ def ask_question(chat_id):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     """Функция для обработки нажатия кнопки ответа пользователем."""
-
+    
     chat_id = call.message.chat.id
     user_info = user_data.get(chat_id)
 
     if user_info:
         question = user_info['questions'][user_info['current_question_index']]
-
-        # Проверяем ответ
         is_correct = call.data == question['answer']
-        if is_correct:
-            user_info['scores'] += 1
-            bot.answer_callback_query(call.id, text='Right!')
-        else:
-            bot.answer_callback_query(call.id, text='Wrong!')
+        response_text = 'Right!' if is_correct else 'Wrong!'
 
-        # Переходим к следующему вопросу
+        # Используем безопасную функцию отправки
+        bot.answer_callback_query(call.id, text=response_text)
+
+        user_info['scores'] += 1 if is_correct else 0
         user_info['current_question_index'] += 1
         ask_question(chat_id)
 
@@ -92,10 +112,9 @@ def time_is_up(chat_id):
 
     user_info = user_data.get(chat_id)
     if user_info:
-        bot.send_message(chat_id=chat_id, text='Time is over!')
+        safe_send_message(chat_id=chat_id, text='Time is over!')
         user_info['current_question_index'] += 1
         ask_question(chat_id)
-
 
 
 @bot.message_handler(commands=['start'])
@@ -107,15 +126,10 @@ def wake_up(message):
     button_newquiz = types.KeyboardButton('/newquiz')
     keyboard.add(button_newquiz)
 
-    bot.send_message(
-        chat_id=chat.id,
-        text=f'Hello, {name}! '
-             'Do you wanna play the quiz about Solidity❔❔❔ \n'
-             'There will be 10 questions with 30 seconds for each. \n'
-             'A very special surprise awaits you at the end of quiz! 🎁 \n'
-             'Push /newquiz 👣',
-        reply_markup=keyboard,
-    )
-
+    safe_send_message(chat_id=chat.id, text=f'Hello, {name}! '
+                                            'Do you wanna play the quiz about Solidity❔❔❔ \n'
+                                            'There will be 10 questions with 30 seconds for each. \n'
+                                            'A very special surprise awaits you at the end of quiz! 🎁 \n'
+                                            'Push /newquiz 👣')
 
 bot.polling()
